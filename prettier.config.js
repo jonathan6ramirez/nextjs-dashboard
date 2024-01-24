8bf0ab19668aa676d3d69d639cc32377202723bf
JonathanRamirez <jonathan@leadrpro.com>
2024-01-24 00:31:17 -0600

    Adding error handling and form validation

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,8 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  trailingComma: 'none',
+  spaceWidth: 4,
+  semi: true,
+  singleQuote: true,
 };
